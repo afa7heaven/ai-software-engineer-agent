ab2f1f9fd050bc03f459e5fcec23c7ef2f4f4ab4
afa7heaven <58665716+afa7heaven@users.noreply.github.com>
2026-05-21 00:16:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ from transformers import pipeline
 
 st.title("🤖 AI Software Engineer")
 
-# MODEL AI GRATIS
 pipe = pipeline(
     "text-generation",
     model="distilgpt2"
 )
 
 mode = st.selectbox("Mode", [
-    "Full Stack Web App",
-    "Mobile App (Flutter)",
-    "Debug Error"
+    "Web App",
+    "Flutter",
+    "Debug"
 ])
 
 task = st.text_area("Masukkan tugas")
